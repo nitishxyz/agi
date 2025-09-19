48ef2c3c7f1af4494d66fbfd0f6aa30ab1c132b1
nitishxyz <nitishxyz@proton.me>
2025-09-19 15:28:03 +0530

    ci(new) new publish workflows

---
+++ infra/script.ts
@@ -0,0 +1,13 @@
+import { domains } from "./domains";
+
+// Cloudflare Worker to serve the install script
+export const script = new sst.cloudflare.Worker("AgiSh", {
+  domain: domains.sh,
+  handler: "infra/handlers/install-worket.ts",
+  build: {
+    loader: {
+      ".sh": "text",
+    },
+  },
+  url: true,
+});
